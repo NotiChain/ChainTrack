4e9458aff6c0b725fd88a4f8454fa353958db27d
Nikolay Shurkalin <9460355@gmail.com>
2023-10-05 20:00:04 +0200

    fix: remove unneeded jsdoc

--- packages/site/src/utils/theme.ts
+++ packages/site/src/utils/theme.ts
@@ -2,12 +2,6 @@ import { getLocalStorage, setLocalStorage } from './localStorage';
 
 type Theme = 'dark' | 'light';
 
-/**
- * Get the user's preferred theme in local storage.
- * Will default to the browser's preferred theme if there is no value in local storage.
- *
- * @returns Theme
- */
 export const getThemePreference = (): Theme => {
   const darkModeSystem = window?.matchMedia(
     '(prefers-color-scheme: dark)',
